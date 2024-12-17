aca73f2d03790cd21d3ef0a0de7958745077bfff
Barış Kaplan <63741832+Baris000-eng@users.noreply.github.com>
2024-12-17 23:24:05 +0300

    mini google translate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from googletrans import Translator
+from googletrans import Translator 
 
 app = Flask(__name__)
 
@@ -11,17 +11,21 @@ def home():
 
 @app.route('/translate', methods=['POST'])
 def translate():
-    # Get the text and language choice from the form
+    # Get the text and target language choice from the form
     text = request.form.get('text')
     target_language = request.form.get('target_language')
 
-    # Translation
     if text:
-        translation = translator.translate(text, dest=target_language)
-        translated_text = translation.text
+        try:
+            # Translate the text to the target language
+            translated_text_object = translator.translate(text, dest=target_language)
+            translated_text = translated_text_object.text
+        except Exception as e:
+            translated_text = f"Error during translation: {e}"
     else:
-        translated_text = None
+        translated_text = "Please provide some text to translate."
 
+     
     return render_template('index.html', translated_text=translated_text, text=text, target_language=target_language)
 
 if __name__ == '__main__':
