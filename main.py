fa11576d14b269fc167775f71d66ecd5f5f8c5c4
Barış Kaplan <63741832+Baris000-eng@users.noreply.github.com>
2024-12-17 23:59:17 +0300

    google translate clone

--- main.py
+++ main.py
@@ -1,78 +1,14 @@
 from flask import Flask, render_template, request
 from googletrans import Translator 
+from google_translate_constants import languages
 
 app = Flask(__name__)
 
 translator = Translator()
 
-langs = {
-    'af': 'Afrikaans',
-    'sq': 'Albanian',
-    'ar': 'Arabic',
-    'hy': 'Armenian',
-    'bn': 'Bengali',
-    'bs': 'Bosnian',
-    'ca': 'Catalan',
-    'hr': 'Croatian',
-    'cs': 'Czech',
-    'da': 'Danish',
-    'nl': 'Dutch',
-    'en': 'English',
-    'eo': 'Esperanto',
-    'et': 'Estonian',
-    'tl': 'Filipino',
-    'fi': 'Finnish',
-    'fr': 'French',
-    'de': 'German',
-    'el': 'Greek',
-    'gu': 'Gujarati',
-    'hi': 'Hindi',
-    'hu': 'Hungarian',
-    'is': 'Icelandic',
-    'id': 'Indonesian',
-    'it': 'Italian',
-    'ja': 'Japanese',
-    'jw': 'Javanese',
-    'ka': 'Georgian',
-    'km': 'Khmer',
-    'ko': 'Korean',
-    'la': 'Latin',
-    'lv': 'Latvian',
-    'lt': 'Lithuanian',
-    'mk': 'Macedonian',
-    'ml': 'Malayalam',
-    'mr': 'Marathi',
-    'my': 'Burmese',
-    'ne': 'Nepali',
-    'no': 'Norwegian',
-    'pl': 'Polish',
-    'pt': 'Portuguese',
-    'ro': 'Romanian',
-    'ru': 'Russian',
-    'sr': 'Serbian',
-    'si': 'Sinhala',
-    'sk': 'Slovak',
-    'sl': 'Slovenian',
-    'es': 'Spanish',
-    'su': 'Sundanese',
-    'sw': 'Swahili',
-    'sv': 'Swedish',
-    'ta': 'Tamil',
-    'te': 'Telugu',
-    'th': 'Thai',
-    'tr': 'Turkish',
-    'uk': 'Ukrainian',
-    'ur': 'Urdu',
-    'vi': 'Vietnamese',
-    'cy': 'Welsh',
-    'xh': 'Xhosa',
-    'yi': 'Yiddish',
-    'zu': 'Zulu'
-}
-
 @app.route('/')
 def home():
-    return render_template('index.html', langs=langs)
+    return render_template('index.html', langs=languages)
 
 @app.route('/translate', methods=['POST'])
 def translate():
@@ -91,7 +27,7 @@ def translate():
         translated_text = "Please provide some text to translate."
 
      
-    return render_template('index.html', translated_text=translated_text, text=text, target_language=target_language, langs=langs)
+    return render_template('index.html', translated_text=translated_text, text=text, target_language=target_language, langs=languages)
 
 if __name__ == '__main__':
     app.run(debug=True)
